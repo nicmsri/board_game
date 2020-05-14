45dfb33be69f74047ee5d8288ac4953dae9d2f82
loco <nicolo.prime@gmail.com>
2020-05-14 13:27:24 +0200

    how to save credentials + overall rework

--- git.sh
+++ git.sh
@@ -1,13 +1,9 @@
 git status # files changed and added in the dir
-
 git diff # differences between dir and repo files
 
-git commit
-
-#Add README.md
-# . . . Commit comments . . .
-
-git push #upload files
+git add <filename> # add file in FilesToPush
+git commit # comments the changes made in FilesToPush
+git push # git upload FilesToPush
 
 git log
 git log --patch
@@ -15,3 +11,5 @@ git log --patch
 git pull # downloads files
 
 # http://ndpsoftware.com/git-cheatsheet.html
+
+git config --global credential.helper store # save git credentials
