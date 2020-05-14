9fb8f9d159a05264f564c691d71a3e24fb060a6f
loco <nicolo.prime@gmail.com>
2020-05-14 12:37:00 +0200

    main git shell commands

---
+++ git.sh
@@ -0,0 +1,17 @@
+git status # files changed and added in the dir
+
+git diff # differences between dir and repo files
+
+git commit
+
+#Add README.md
+# . . . Commit comments . . .
+
+git push #upload files
+
+git log
+git log --patch
+
+git pull # downloads files
+
+# http://ndpsoftware.com/git-cheatsheet.html
